39b2b73a0d27a22e0a9314c20ca66e2f6dcbfc29
Ryota Ushio <72342591+RyotaUshio@users.noreply.github.com>
2023-12-18 21:26:43 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab } from './settings';
+
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+		await this.saveSettings();
+		this.addSettingTab(new SampleSettingTab(this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
