47aa804bf25283b18bc1991641c90fa5e0a4a331
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-01-07 21:47:38 +0900

    improved hover editor handling

--- src/patchers/page-preview.ts
+++ src/patchers/page-preview.ts
@@ -10,10 +10,11 @@ export const patchPagePreview = (plugin: PDFPlus): boolean => {
     const app = plugin.app;
     const pagePreview = app.internalPlugins.plugins['page-preview'].instance;
     
-    // Make sure this plugin gets loaded after Hover Editor, because it completely overrides the `onLinkHover` method
+    // Make sure this plugin patches `onLinkHover` after Hover Editor, because it completely overrides the original method
     if (app.plugins.enabledPlugins.has('obsidian-hover-editor')) {
         const hoverEditor = app.plugins.plugins['obsidian-hover-editor']; // this is set after loading Hover Editor
         if (!hoverEditor) return false;
+        if (!pagePreview.onLinkHover.name) return false;
     }
 
     plugin.register(around(pagePreview.constructor.prototype, {
