e352e09a2e78a56161d5628357db1a63c639e7a3
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-02-24 13:49:39 +0900

    docs test

---
+++ docs/lib/scripts/graph-data.js
@@ -0,0 +1,1 @@
+let graphData={graphOptions:{attractionForce:1,linkLength:10,repulsionForce:150,centralForce:3,edgePruning:100,minNodeRadius:3,maxNodeRadius:7},isInitialized:!0,paths:["index.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["Home"],radii:[null],linkCount:0}
